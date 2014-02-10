ea476fd1440edc85bf6939869ac4c7744cd4f4b6
Pushparaj Zala <pushparaj@objectlounge.com>
2014-02-10 15:24:17 +0530

    Facebook Test Code
    Facebook test code with library.
    Library files is in folder FacebookCode/FacebookLibrary

---
+++ Facebook/SampleFacebookCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FacebookLibrary
+//
+//  Created by Pushparaj Zala on 2/7/14.
+//  Copyright (c) 2014 Pushparaj Zala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FacebookAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FacebookAppDelegate class]));
+    }
+}
