dbf0f1ed756cfc5966cbbc5dae4029844bd690e7
Pushparaj Zala <pushparaj@objectlounge.com>
2014-02-11 11:51:11 +0530

    Facebook login library
    Folder Library has library files.
    Folder SampleFacebookCode has a sample code to check library file.

---
+++ SampleFacebookCode/SampleFacebookCode/FBAppDelegate.m
@@ -0,0 +1,48 @@
+//
+//  FBAppDelegate.m
+//  SampleFacebookCode
+//
+//  Created by Pushparaj Zala on 2/11/14.
+//  Copyright (c) 2014 Pushparaj Zala. All rights reserved.
+//
+
+#import "FBAppDelegate.h"
+#import <FacebookSDK/FacebookSDK.h>
+#import "FacebookLogin.h"
+
+@implementation FBAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    //self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    //self.window.backgroundColor = [UIColor whiteColor];
+    //[self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    // Note this handler block should be the exact same as the handler passed to any open calls.
+    [FBSession.activeSession setStateChangeHandler:
+     ^(FBSession *session, FBSessionState state, NSError *error) {
+         // Retrieve the app delegate
+         //         FacebookAppDelegate* appDelegate = [UIApplication sharedApplication].delegate;
+         // Call the app delegate's sessionStateChanged:state:error method to handle session state changes
+         //FacebookLogin *cv=[[FacebookLogin alloc]init];
+         [FacebookLogin sessionStateChanged:session state:state error:error];
+     }];
+    
+    return [FBSession.activeSession handleOpenURL:url];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActive];
+}
+
+@end
